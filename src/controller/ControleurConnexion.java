0efcedb11e5d13c33526dbaf11dbe98a8daddac7
Vitally <u4424054260@gmail.com>
2026-02-25 21:30:51 +0100

    [FEAT] ControleurConnexion.java - Travail en cours : code non abouti et non fonctionnel

--- src/controller/ControleurConnexion.java
+++ src/controller/ControleurConnexion.java
@@ -1,22 +1,26 @@
 package controller;
+
+// WIP (Work In Progress) : connexion BDD pas encore implémentée
 import java.sql.*;
 
+/**
+ * Contrôleur de connexion : gère les actions depuis la vue Connexion.
+ * Reçoit un choix (1 = connexion, 2 = création de compte).
+ */
 public class ControleurConnexion {
-	public ControleurConnexion(int choix) {
-			
-		// Si le user a cliqué sur le bouton "connexion" :
-		if(choix == 1) {
-			System.out.println("totototototottatatatatatttttitititiititit");
-		// --> Comparer les chaînes de caractères avec celles de la BDD (isset et !empty)
-		// --> Si OK : affichage de la vue Accueil
-		
-		
-		}
-	
-		// Si le user a cliqué sur le bouton "creation" :
-		if(choix == 2) {
-		// --> Affichage de la vue Création de Compte
-		
-		}
-}
+
+    public ControleurConnexion(int choix) {
+
+        // Bouton "connexion" de la vue Connexion.java
+        if (choix == 1) {
+            System.out.println("totototototottatatatatatttttitititiititit");
+            // TODO : comparer identifiant/mdp avec la BDD
+            // TODO : si OK → afficher la vue Accueil
+        }
+
+        // Bouton "creation" de la vue Connexion.java
+        if (choix == 2) {
+            // TODO : afficher la vue Création de Compte
+        }
+    }
 }

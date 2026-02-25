312f9f529b58cc3846a3b5b92f373261bfb6f279
Vitally <u4424054260@gmail.com>
2026-02-25 21:28:23 +0100

    [FEAT] Connexion.java - Code abouti et fonctionnel

--- src/view/Connexion.java
+++ src/view/Connexion.java
@@ -1,91 +1,69 @@
 package view;
-import javafx.scene.Parent;
 
+// Imports JavaFX - Layout
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.Priority;
+
+// Imports JavaFX - Composants UI
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.PasswordField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundImage;
-import javafx.scene.layout.BackgroundPosition;
-import javafx.scene.layout.BackgroundRepeat;
-import javafx.scene.layout.BackgroundSize;
-import javafx.scene.layout.VBox;
-import javafx.scene.layout.Priority;
-import controller.ControleurConnexion;
+
+// Imports JavaFX - Géométrie
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 
+// Import contrôleur
+import controller.ControleurConnexion;
+
+/**
+ * Vue de connexion : écran d'accueil avec identifiant, mot de passe,
+ * boutons de connexion et de création de compte.
+ */
 public class Connexion extends Scene {
-	public static String valeur;
 
-	public Connexion(VBox vbox) {
-		super(vbox, 800, 600);
-		
-		Image lapin = new Image(getClass().getResourceAsStream("/lapin.png"));
-		Button connexion = new Button("Se connecter");
-		Button creation = new Button("Créer un compte");
-		Label etiquette = new Label("Bienvenue dans JavaFX !");
-		TextField identifiant = new TextField();
-		PasswordField mdp = new PasswordField();
-				
-		vbox.setPadding(new Insets(30));
-		vbox.setMaxWidth(350);
-		vbox.setAlignment(Pos.CENTER);
-		vbox.setStyle("""
-			    -fx-background-image: url("/lapin.png");
-			    -fx-background-size: cover;
-				-fx-background-position: center;
-				""");
-		
-		BackgroundPosition bckP = new BackgroundPosition(null,
-				getWidth(),
-				isDepthBuffer(),
-				null,
-				getHeight(),
-				isDepthBuffer());
-		
-		BackgroundSize bckS = new BackgroundSize(BackgroundSize.AUTO,
-				BackgroundSize.AUTO,
-				false,
-				false,
-				true,
-				false);
-		
-		Background bck = new Background(new BackgroundImage(lapin, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, bckP, bckS));
-		
-		vbox.setBackground(bck);
-		
-		vbox.getChildren().add(etiquette);		
-		
-		vbox.getChildren().add(identifiant);
-		vbox.getChildren().add(mdp);
-		
-		vbox.getChildren().add(connexion);
-		connexion.setOnAction(e -> {
-        	new ControleurConnexion(1);            
-        });
-		
-		vbox.getChildren().add(creation);
-		creation.setOnAction(e -> {
-			new ControleurConnexion(2);   
-        });
-		
-		// Personnalisation du VBox
-		vbox.setSpacing(10);
-		vbox.setPadding(new Insets(20, 20, 20, 20));
-		
-		// Taille des children de vbox
-		identifiant.setMaxWidth(200);
-		mdp.setMaxWidth(200);
-		connexion.setMaxWidth(200);
-		creation.setMaxWidth(200);
-		
-		// Adaptation à la taille de la scène
-		VBox.setVgrow(etiquette, Priority.ALWAYS);
-		VBox.setVgrow(connexion, Priority.ALWAYS);
+    public static String valeur;
 
-	}
+    public Connexion(VBox vbox) {
+        super(vbox, 800, 600);
+
+        // --- Création des composants UI ---
+        Label etiquette = new Label("Bienvenue dans JavaFX !");
+        TextField identifiant = new TextField();
+        PasswordField mdp = new PasswordField();
+        Button connexion = new Button("Se connecter");
+        Button creation = new Button("Créer un compte");
+
+        // --- Image de fond via CSS ---
+        vbox.setStyle("""
+                -fx-background-image: url("/lapin.png");
+                -fx-background-size: cover;
+                -fx-background-position: center;
+                """);
+
+        // --- Mise en page du VBox ---
+        vbox.setAlignment(Pos.CENTER);
+        vbox.setMaxWidth(350);
+        vbox.setSpacing(10);
+        vbox.setPadding(new Insets(20));
+
+        // --- Taille max des composants ---
+        identifiant.setMaxWidth(200);
+        mdp.setMaxWidth(200);
+        connexion.setMaxWidth(200);
+        creation.setMaxWidth(200);
+
+        // --- Croissance verticale (adaptation à la fenêtre) ---
+        VBox.setVgrow(etiquette, Priority.ALWAYS);
+        VBox.setVgrow(connexion, Priority.ALWAYS);
+
+        // --- Ajout des composants dans l'ordre d'affichage ---
+        vbox.getChildren().addAll(etiquette, identifiant, mdp, connexion, creation);
+
+        // --- Événements ---
+        connexion.setOnAction(e -> new ControleurConnexion(1));
+        creation.setOnAction(e -> new ControleurConnexion(2));
+    }
 }

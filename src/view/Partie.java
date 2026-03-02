16d0363284bdaeac5ba7a3a3f7812daa12e559b4
AllanC229 <allan.chvt@gmail.com>
2026-03-02 16:27:44 +0100

    ajout d'un bandeau de fond à la partie

--- src/view/Partie.java
+++ src/view/Partie.java
@@ -9,11 +9,19 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
 import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.RowConstraints;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class Partie extends Scene {
 	
@@ -46,6 +54,36 @@ public class Partie extends Scene {
 	Pane zonecentrale = new Pane();			//Ces deux lignes servent à regrouper les 3 cases de la ligne du milieu  en une seule grand case
 	partie.add(zonecentrale, 0, 1, 3, 1);	//On l'ajoute directement ici  à "partie"
 	
+	/*Image bando = new Image(getClass().getResource("/bandoHunvre.jpg").toExternalForm());
+	ImageView fondzonesup = new ImageView(bando);		//permet d'afficher l'image comme un fond dans le bandeau superieur
+	partie.add(fondzonesup,  0, 0);*/
+	
+	Pane zonesuperieure = new Pane();
+
+	Image image = new Image(getClass().getResource("/bandoHunvre.jpg").toExternalForm());
+
+	BackgroundImage bg = new BackgroundImage(
+	        image,
+	        BackgroundRepeat.NO_REPEAT,
+	        BackgroundRepeat.NO_REPEAT,
+	        BackgroundPosition.CENTER,
+	        new BackgroundSize(
+	                100, 100,
+	                true, true,
+	                true, false
+	        )
+	);
+
+	zonesuperieure.setBackground(new Background(bg));
+
+	partie.add(zonesuperieure, 0, 0, 3, 1);
+	
+	
+	
+	Pane zonecarte = new Pane();		//On définit ici la zone qui sert à afficher les cartes de la main du joueur
+	partie.add(zonecarte, 1, 2);		//On la positionne sur la deuxième colonne de la troisième ligne
+	Label test = new Label("hunvre");
+	zonecarte.getChildren().add(test);
 	
 	Button bquitter = new Button("Quitter le jeu");		//bouton pour fermer l'application
 	VBox.setMargin(bquitter, new Insets(50, 0, 0, 0));
@@ -59,6 +97,9 @@ public class Partie extends Scene {
 
 		alerte.setTitle("Vous allez quitter le jeu");
 		alerte.setHeaderText(null);
+		
+		Stage stage = (Stage) bquitter.getScene().getWindow();
+	    alerte.initOwner(stage);
 
 		if (alerte.showAndWait().get() == ButtonType.YES) {		//si on clique oui sur la fenetre
 		    Platform.exit();	//ferme l'application

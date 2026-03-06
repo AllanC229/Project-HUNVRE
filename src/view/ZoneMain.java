ce6981768e2388ba4404aeb44c19b7bbb94a4b71
Vitally <u4424054260@gmail.com>
2026-03-06 18:06:46 +0100

    [FEAT] ZoneMain.java bouton Jouer et bouton Jeter

--- src/view/ZoneMain.java
+++ src/view/ZoneMain.java
@@ -2,6 +2,9 @@ package view;
 
 import javafx.scene.control.Label;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import controller.ControleurConnexion;
+import javafx.scene.control.Button;
 
 public class ZoneMain extends Pane {
 	
@@ -9,6 +12,27 @@ public class ZoneMain extends Pane {
 	
 	Label test = new Label("hunvre");
 	this.getChildren().add(test);
+	
+	// Début code Vitally - Boutons Jouer et Bouton Jeter
+	
+		// 	--- Boutons ---
+		Button jouer = new Button("Jouer");
+		Button jeter = new Button("Jeter");
+		     
+		// --- Taille max des composants ---
+		jouer.setMaxWidth(200);
+		jeter.setMaxWidth(200);
+		
+		// --- Événements ---
+        jouer.setOnAction(e -> {
+            // à écrire
+        });
+
+        jeter.setOnAction(e -> {
+        	// à écrire
+        });
+
+ 	// Fin code Vitally - Boutons Jouer et Bouton Jeter
 
 }
 }

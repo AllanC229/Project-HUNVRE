d61e4a96a9549d48ffd239b36e87b201485d4686
JeromeA-greta <jerome.anamoutou@gmail.com>
2026-04-21 16:02:22 +0200

    [FIX] bouton Infos de ZoneMenu

--- src/view/ZoneMenu.java
+++ src/view/ZoneMenu.java
@@ -135,7 +135,7 @@ public class ZoneMenu extends Pane {
 				System.out.println("bcartes cliqué");
 				stageinfos.close();
 				DeckJoueur deckListe = ControleurConnexion.joueur.getDeck();
-				int tailleDeckInfos = deckListe.getListedeck().size();
+				int tailleDeckInfos = deckListe.size();
 				System.out.println(tailleDeckInfos + " cartes dans le deck");
 				
 				HBox ligneTrefle = new HBox();
@@ -157,7 +157,8 @@ public class ZoneMenu extends Pane {
 				
 				// Compte du nombre de cartes par couleur
 				for(int j = 0; j < tailleDeckInfos; j++) {
-					switch (((CarteJeu) deckListe.getListedeck().get(j)).getCouleur()) {
+					CarteJeu carte = (CarteJeu) deckListe.get(j);
+					switch (carte.getCouleur()) {
 					case "trefle":
 						compteTrefle += 1;
 						break;
@@ -175,7 +176,7 @@ public class ZoneMenu extends Pane {
 				
 				for(int i = 0; i < tailleDeckInfos; i++) {
 					
-					CarteJeu carteListe = (CarteJeu) deckListe.getListedeck().get(i);
+					CarteJeu carteListe = (CarteJeu) deckListe.get(i);
 					Image imageCarte = new Image(getClass().getResourceAsStream("/" + carteListe.getRecto() + ".jpg"));
 					ImageView lacarte = new ImageView(imageCarte);
 					lacarte.setFitWidth(60);
@@ -228,6 +229,46 @@ public class ZoneMenu extends Pane {
 				});
 				
 				stageListeCartes.show();
+			});
+			
+			bcombinaisons.setOnAction(em -> {
+				System.out.println("bcombinaisons cliqué");
+				stageinfos.close();
+				
+				Label combo1 = new Label("Carte haute : Si aucune autre combinaison n'est présente, alors la carte ayant la plus haute valeur sera prise en compte - placeholderpoints");
+				Label combo2 = new Label("Paire : Deux cartes de même valeur - placeholderpoints");
+				Label combo3 = new Label("Double paire : Deux cartes de même valeur avec deux autres cartes de même valeur - placeholderpoints");
+				Label combo4 = new Label("Brelan : Trois cartes de même valeur - placeholderpoints");
+				Label combo5 = new Label("Carré : Quatre cartes de même valeur - placeholderpoints");
+				Label combo6 = new Label("Suite : Cinq cartes à la suite avec au moins une couleur différente - placeholderpoints");
+				Label combo7 = new Label("Couleur : Cinq cartes de même couleur - placeholderpoints");
+				Label combo8 = new Label("Main pleine : Trois cartes de même valeur et deux cartes de même valeur. Les trois cartes et les deux cartes ne sont pas de la même couleur - placeholderpoints");
+				Label combo9 = new Label("Quinte Flush : Cinq cartes à la suite et de même couleur - placeholderpoints");
+				Label combo10 = new Label("Quinte Flush Royale : Une suite avec l'As, le Roi, la Dame, le Valet et le 10, tous de la même couleur - placeholderpoints");
+				
+				
+				Button bfermerInfoCombo = new Button("Fermer");
+				VBox ligneCombo = new VBox();
+				ligneCombo.setSpacing(10);
+				ligneCombo.setAlignment(Pos.CENTER);
+				ligneCombo.getChildren().addAll(combo1,combo2,combo3,combo4,combo5,combo6,combo7,combo8,combo9,combo10,bfermerInfoCombo);
+				
+				Scene sceneInfoCombo = new Scene(ligneCombo, 950, 450);
+				Stage stageInfoCombo = new Stage();
+				stageInfoCombo.setTitle("Liste des cartes");
+				stageInfoCombo.setResizable(false);
+				stageInfoCombo.setScene(sceneInfoCombo);
+				
+				Stage stageCombo = (Stage) binfos.getScene().getWindow();
+				stageInfoCombo.initOwner(stageCombo);
+				
+				bfermerInfoCombo.setOnAction(efermer -> {
+					System.out.println("bouton bfermerInfoCartes cliqué");
+					stageInfoCombo.close();
+				});
+				
+				stageInfoCombo.show();
+				
 			});
 			
 			breprendreInfos.setOnAction(el -> {

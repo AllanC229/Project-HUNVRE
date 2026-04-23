b7ebab5bc0a31cce4a92ab1023d99d27cf37a9c8
JeromeA-greta <jerome.anamoutou@gmail.com>
2026-04-23 14:36:16 +0200

    [FIX]import dans ControleurPartie, ZoneMenu

--- src/controller/ControleurPartie.java
+++ src/controller/ControleurPartie.java
@@ -2,9 +2,12 @@ package controller;
 
 // --- java.* ---
 import java.util.List;
+import java.sql.*;
 
+import connection.DAOAcces;
 // --- Classes du projet ---
 import model.CarteJeu;
+import model.Utilisateur;
 import view.ZoneCentrale;
 import view.ZoneDeck;
 import view.ZoneScore;
